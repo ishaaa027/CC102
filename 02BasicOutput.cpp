9ea8ec2fad3a82bbad95abb86e94da658ffbaaa5
Krissha <belenisha35@gmail.com>
2024-10-03 09:37:59 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "BSIT 1-A" << endl;
+	cout << "Krissha G. Belen" << endl;
+	cout << "Vinzons, Camarines Norte" << endl;
+	
+return 0;
+
+}
